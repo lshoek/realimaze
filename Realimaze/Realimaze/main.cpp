13eeca2e5958a9b944aaccaee00084dd230c4d13
lshoek <lesleymartina@hotmail.com>
2014-05-16 16:37:23 +0200

    initial project setup, including glut

--- Realimaze/Realimaze/main.cpp
+++ Realimaze/Realimaze/main.cpp
@@ -1,4 +1,11 @@
-int main(int argc, const char* argv[])
+#include <string>
+#include <iostream>
+#include "Manager.h"
+
+using namespace std;
+
+//Main function launches the game
+int main()
 {
-	
+	Manager testMngr{};
 }
